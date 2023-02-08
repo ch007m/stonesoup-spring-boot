8dbf45ee48083936f79ac0138f030acdfe556064
Charles Moulliard <cmoulliard@redhat.com>
2023-02-08 17:16:41 +0100

    Review the code to pass an env variable

--- src/main/java/com/example/demo/DemoApplication.java
+++ src/main/java/com/example/demo/DemoApplication.java
@@ -1,5 +1,6 @@
 package com.example.demo;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -12,8 +13,11 @@ public class DemoApplication {
 
     @RequestMapping("/")
     String home() {
-        return "Hello World!";
+        return String.format("Welcome to the %s bar !",bar);
     }
+
+    @Value("${bar:jungle}")
+    private String bar;
 
     public static void main(String[] args) {
         SpringApplication.run(DemoApplication.class, args);
